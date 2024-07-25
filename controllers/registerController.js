9840d584c82cb6b5832bd5e3825895385e1b91db
ruhamabek <ruhamauzubo@gmail.com>
2024-07-25 11:15:01 +0300

    Adding user authetication handler

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,29 +1,32 @@
 const usersDB = {
   users: require('../model/users.json'),
-  setUsers: function (data) {this.users = data}
+  setUsers: function (data) { this.users = data }
 }
 const fsPromises = require('fs').promises;
 const path = require('path');
-const bcrpt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
-const handleNewUser = async (req,res)=>{
-  const {user , pwd} = req.body;
-  if(!user || !pwd) {
-    return res.status(400).json({'message' : 'username and password required'});
+const handleNewUser = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+  // check for duplicate usernames in the db
+  const duplicate = usersDB.users.find(person => person.username === user);
+  if (duplicate) return res.sendStatus(409); //Conflict 
+  try {
+      //encrypt the password
+      const hashedPwd = await bcrypt.hash(pwd, 10);
+      //store the new user
+      const newUser = { "username": user, "password": hashedPwd };
+      usersDB.setUsers([...usersDB.users, newUser]);
+      await fsPromises.writeFile(
+          path.join(__dirname, '..', 'model', 'users.json'),
+          JSON.stringify(usersDB.users)
+      );
+      console.log(usersDB.users);
+      res.status(201).json({ 'success': `New user ${user} created!` });
+  } catch (err) {
+      res.status(500).json({ 'message': err.message });
   }
-  const duplicate = usersDB.users.find((person)=>{person.username === user});
-  if(duplicate){
-    return res.sendStatus(409);
-  }
-  try{
-    const hashedPwd = await bcrpt.hash(pwd , 10);
-    const newUser = {"username" : user , "password" : hashedPwd};
-    usersDB.setUsers([...usersDB , newUser]);
-    await fsPromises.writeFile(path.join(__dirname , '..', 'model' , 'user.json') , JSON.stringify(usersDB.users)
-    );
-    console.log(usersDB.users);
-    res.status(201).json({'sucess' : `new user ${user} created`});  
-  } catch(err){
-    res.status(500).json({'message' : err.message});
-  }
-} 
+}
+
+module.exports = { handleNewUser };
