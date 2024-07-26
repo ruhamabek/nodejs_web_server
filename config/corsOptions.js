9651a3f4b31edac455e36ae4cd2b23ebaa1d3e79
ruhamabek <ruhamauzubo@gmail.com>
2024-07-26 16:03:38 +0300

    configuring the JWT autheticattion

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,11 +1,8 @@
-const whitelist = ['https://www.yoursite.com', 
-                  'http://127.0.0.1:5500', 
-                  'http://localhost:3500'
-                  ];
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
@@ -14,4 +11,4 @@ const corsOptions = {
     optionsSuccessStatus: 200
 }
 
-module.exports = {corsOptions};
+module.exports = corsOptions;
